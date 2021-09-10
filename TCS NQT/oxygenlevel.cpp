f518c02f64ad87cb27f241ef70089708b502dc79
sayancse <78550096+code-say@users.noreply.github.com>
2021-09-10 19:30:53 +0530

    Update oxygenlevel.cpp

--- TCS NQT/oxygenlevel.cpp
+++ TCS NQT/oxygenlevel.cpp
@@ -17,5 +17,25 @@ int main()
             }
         }
     }
+    for(i =0 ; i<3 ; i++){
+        for(j =0 ; j<3 ; j++){
+            average[i] = average[i] + trainee[j][i];
+        }
+        average[i] = average[i] / 3;
+    }
+    for(i=0 ; i<3; i++){
+        if(average[i]>max){
+            max = average[i];
+        }
+    }
+    for(i=0 ; i<3 ; i++){
+        if(average[i]==max){
+            cout << "Trainee Number : "<<i+1<<"\n";
+        }
+        if(average[i]<70){
+            cout<<"Trainee is Unfit";
+        }
+    }
+    return 0;
     
 }
