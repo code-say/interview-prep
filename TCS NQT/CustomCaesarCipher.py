437d10a2d5f59e853c931fb59eda46d60b08541c
sayancse <78550096+code-say@users.noreply.github.com>
2021-09-17 19:31:02 +0530

    Add files via upload

---
+++ TCS NQT/CustomCaesarCipher.py
@@ -0,0 +1,20 @@
+def caesar(text,key):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) + key-65) % 26 + 65)
+        elif(char.islower()):
+            result += chr((ord(char) + key - 97) % 26 + 97)
+        elif(char.isdigit()):
+            result += str(int(char) + key)
+        elif(char == '-'):
+            result += '-'
+        elif (char.isspace()):
+            result += " "
+    return result
+
+
+text = input("Enter your plain text:")
+key = int(input("Enter the key: "))
+print(caesar(text,key))
