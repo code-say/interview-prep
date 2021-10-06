d5d4be8bca9050e7f1d4c9a11fcafe67cee70016
sayancse <78550096+code-say@users.noreply.github.com>
2021-10-06 09:38:06 +0530

    Create linear_search.js

---
+++ javascript/linear_search.js
@@ -0,0 +1,13 @@
+Linear Search Solution (without Recursion)
+
+function linearSearchIndexOf(arr, val)
+{
+  for(let i=0; i<arr.length; i++)
+{
+  if(arr[i]===val)
+  {
+    return i;
+  }
+}
+return -1;
+}
