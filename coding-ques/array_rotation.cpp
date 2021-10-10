5ff51647c595ad36a1dc357393921ac23b37ae47
sayancse <78550096+code-say@users.noreply.github.com>
2021-10-10 17:16:26 +0530

    Create array_rotation.cpp

---
+++ coding-ques/array_rotation.cpp
@@ -0,0 +1,34 @@
+// Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements. 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* function to left Roatate arr[] of size n by 1*/
+
+void leftRoatatebyOne(int arr[], int n)
+{
+  int temp = arr[0], i;
+  for (i=0; i< n; i++)
+     arr[i] = arr[i+1];
+  
+  arr[n-1] = temp;
+}
+
+/* function to left Roatate arr[] of size n by d*/
+void leftRoatae(int arr[], int d, int n)
+{
+  for(int i=0;i < d; i++)
+    leftRotatebyOne(arr, n);
+}
+
+/*utility function to print an array */
+void printArray(int arr[], int n)
+{
+  for(int i = 0; i< n; i++)
+    cout << arr[i] << " ";
+}
+
+int main()
+{
+  
+}
