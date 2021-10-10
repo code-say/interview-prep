2f295b34cc58817b2fe1a136f4a75bf8a0ed4928
sayancse <78550096+code-say@users.noreply.github.com>
2021-10-10 17:18:32 +0530

    Update array_rotation.cpp

--- coding-ques/array_rotation.cpp
+++ coding-ques/array_rotation.cpp
@@ -30,5 +30,12 @@ void printArray(int arr[], int n)
 
 int main()
 {
+  int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+  int n = sizeof(arr) / sizeof(arr[0]);
   
+  //Function calling
+  leftRoatate(arr, 2, n);
+  printArray(arr, n);
+  
+  return 0;
 }
