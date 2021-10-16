8f766cc425dd4c7184403960690215a01a60ec18
sayancse <78550096+code-say@users.noreply.github.com>
2021-10-16 17:53:59 +0530

    Update inheritance.cpp

--- c++/inheritance.cpp
+++ c++/inheritance.cpp
@@ -1,1 +1,28 @@
 
+#include<iostream>
+using namespace std;
+
+#define NAME_SIZE 50
+
+class Person {
+    int id;
+    char name[NAME_SIZE];
+public:
+    void aboutMe(){
+        cout << "I am a Person";
+    }
+};
+
+class Student : public Person {
+    public:
+        void aboutMe(){
+            cout << "I am a Student";
+        }
+};
+
+int main(){
+    Student * p = new Student();
+    p->aboutMe();
+    delete p;
+    return 0;
+}
